71a680ca0a82df538850a18fa77d0a73a7ef9c55
chavezalarcon2021-del <chavezalarcon2021@gmail.com>
2026-08-08 00:28:38 -0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,74 +1,159 @@
-const CACHE_NAME = "arm-pyramid-v1";
+const CACHE_NAME =
+    "arm-pyramid-v5";
 
-const FILES_TO_CACHE = [
+const APP_FILES = [
+
     "./",
+
     "./index.html",
+
     "./style.css",
+
     "./script.js",
+
     "./manifest.json",
+
     "./icon-192.png",
+
     "./icon-512.png"
+
 ];
 
-self.addEventListener("install", event => {
 
-    event.waitUntil(
-        caches
-            .open(CACHE_NAME)
-            .then(cache => {
-                return cache.addAll(
-                    FILES_TO_CACHE
-                );
-            })
-    );
+/* INSTALL */
 
-});
+self.addEventListener(
+    "install",
+    event => {
 
+        event.waitUntil(
 
-self.addEventListener("activate", event => {
+            caches
+                .open(
+                    CACHE_NAME
+                )
+                .then(
+                    cache => {
 
-    event.waitUntil(
-        caches.keys().then(cacheNames => {
-
-            return Promise.all(
-
-                cacheNames.map(cache => {
-
-                    if (
-                        cache !== CACHE_NAME
-                    ) {
-
-                        return caches.delete(
-                            cache
+                        return cache.addAll(
+                            APP_FILES
                         );
 
                     }
+                )
 
-                })
-
-            );
-
-        })
-    );
-
-});
+        );
 
 
-self.addEventListener("fetch", event => {
+        self.skipWaiting();
 
-    event.respondWith(
+    }
+);
 
-        caches
-            .match(event.request)
-            .then(response => {
 
-                return (
-                    response ||
-                    fetch(event.request)
-                );
+/* ACTIVATE */
 
-            })
+self.addEventListener(
+    "activate",
+    event => {
 
-    );
+        event.waitUntil(
 
-});
+            caches
+                .keys()
+                .then(
+                    cacheNames => {
+
+                        return Promise.all(
+
+                            cacheNames
+                            .filter(
+                                cacheName =>
+                                    cacheName !==
+                                    CACHE_NAME
+                            )
+                            .map(
+                                cacheName =>
+                                    caches.delete(
+                                        cacheName
+                                    )
+                            )
+
+                        );
+
+                    }
+                )
+
+        );
+
+
+        self.clients.claim();
+
+    }
+);
+
+
+/* FETCH */
+
+self.addEventListener(
+    "fetch",
+    event => {
+
+        if (
+            event.request.method
+            !==
+            "GET"
+        ) {
+
+            return;
+
+        }
+
+
+        event.respondWith(
+
+            fetch(
+                event.request
+            )
+            .then(
+                networkResponse => {
+
+                    const clone =
+                        networkResponse
+                        .clone();
+
+
+                    caches
+                        .open(
+                            CACHE_NAME
+                        )
+                        .then(
+                            cache => {
+
+                                cache.put(
+                                    event.request,
+                                    clone
+                                );
+
+                            }
+                        );
+
+
+                    return networkResponse;
+
+                }
+            )
+            .catch(
+                () => {
+
+                    return caches.match(
+                        event.request
+                    );
+
+                }
+            )
+
+        );
+
+    }
+);
